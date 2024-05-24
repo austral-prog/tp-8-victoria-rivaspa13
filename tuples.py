8fc9efcb9cd656f2a618b6cc1b46da70e5a30da9
victoria-rivaspa13 <162721841+victoria-rivaspa13@users.noreply.github.com>
2024-05-24 11:59:02 -0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,32 +1,17 @@
-"""Functions to help Azara and Rui locate pirate treasure."""
-
-
 def get_coordinate(record):
-    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
-
-    :param record: tuple - with a (treasure, coordinate) pair.
-    :return: str - the extracted map coordinate.
-    """
-
-    return ""
-
+    return record[1]
 
 def convert_coordinate(coordinate):
-    """Split the given coordinate into tuple containing its individual components.
-
-    :param coordinate: str - a string map coordinate
-    :return: tuple - the string coordinate split into its individual components.
-    """
-
-    return ()
+    return (coordinate[0], coordinate[1])
 
 
 def create_record(azara_record, rui_record):
-    """Combine the two record types (if possible) and create a combined record group.
+    treasure_name, treasure_coordinate = azara_record
+    location_name, location_coordinate, quadrant = rui_record
 
-    :param azara_record: tuple - a (treasure, coordinate) pair.
-    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
-    :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
-    """
+    formatted_coordinate = convert_coordinate(treasure_coordinate)
 
-    return ()
+    if formatted_coordinate == location_coordinate:
+        return (treasure_name, treasure_coordinate, location_name, location_coordinate, quadrant)
+    else:
+        return "not a match"
